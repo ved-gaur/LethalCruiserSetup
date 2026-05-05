ed79441ab22dbd68ad4b98294aa54cf3372b59a4
ved-gaur <vedgaur05@gmail.com>
2026-05-05 04:50:26 +0100

    detect all tools on the cruiser and ship

--- CruiserSetup.cs
+++ CruiserSetup.cs
@@ -5,6 +5,8 @@ using ChatCommandAPI;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.InputSystem.Interactions;
 
 namespace CruiserSetup;
 
@@ -85,7 +87,8 @@ internal static class SetupManager
 
         CruiserSetup.Logger.LogInfo($"Found cruiser: {cruiser.name}");
 
-        // TODO: SETUP LOGIC
+        // Detect tools.
+        List<DetectedTool> tools = ToolDetector.FindTools();
     }
 
     private static GameObject FindCruiserOrThrow()
@@ -100,5 +103,85 @@ internal static class SetupManager
         }
 
         return cruiser;
+    }
+}
+
+internal enum ToolLocation
+{
+    Ship,
+    Cruiser
+}
+
+internal readonly struct DetectedTool(GrabbableObject item, ToolLocation location)
+{
+    public GrabbableObject Item { get; } = item;
+    public ToolLocation Location { get; } = location;
+}
+
+internal static class ToolDetector
+{
+    private const string ShipPath = "/Environment/HangarShip";
+    private const string CruiserPath = "CompanyCruiser(Clone)";
+
+    private static readonly HashSet<string> ToolNames =
+    [
+        "Walkie-talkie",
+        "Flashlight",
+        "Shovel",
+        "Lockpicker",
+        "Pro-flashlight",
+        "Stun grenade",
+        "Boombox",
+        "TZP-Inhalant",
+        "Zap gun",
+        "Jetpack",
+        "Extension ladder",
+        "Radar-booster",
+        "Spray paint",
+        "Weed killer",
+        "Shotgun",
+        "Kitchen knife"
+    ];
+
+    public static List<DetectedTool> FindTools()
+    {
+        List<DetectedTool> tools = [];
+
+        AddToolsFromRoot(tools, ShipPath, ToolLocation.Ship);
+        AddToolsFromRoot(tools, CruiserPath, ToolLocation.Cruiser);
+
+        return [.. tools
+            .GroupBy(tool => tool.Item)
+            .Select(group => group.First())];
+    }
+
+    private static void AddToolsFromRoot(
+        List<DetectedTool> tools,
+        string rootPath,
+        ToolLocation location)
+    {
+        GameObject root = GameObject.Find(rootPath);
+
+        if (root == null)
+            return;
+
+        GrabbableObject[] items =
+            root.GetComponentsInChildren<GrabbableObject>(includeInactive: true);
+
+        foreach (GrabbableObject item in items)
+        {
+            if (!IsToolOfInterest(item))
+                continue;
+
+            tools.Add(new DetectedTool(item, location));
+        }
+    }
+
+    private static bool IsToolOfInterest(GrabbableObject item)
+    {
+        if (item == null || item.itemProperties == null)
+            return false;
+
+        return ToolNames.Contains(item.itemProperties.itemName);
     }
 }
